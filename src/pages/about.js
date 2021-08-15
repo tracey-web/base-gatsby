fcdb7349b1496432298a48a5af967bc733f0b225
Tracey <tracey@coch.pro>
2021-08-15 16:38:04 +1000

    Created new shell for frontnet with Gatsby.

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const About = () => {
+  return (
+    <Layout pageHeading="About Us" pageTitle="About">
+      <p>What about us?</p>
+    </Layout>
+  );
+};
+
+export default About;
---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const ContactPage = () => {
+  return (
+    <Layout pageHeading="Contact Us" pageTitle="Contact">
+      <p> Testing the way to do this</p>
+      <p>Phone us!</p>
+    </Layout>
+  );
+};
+
+export default ContactPage;
